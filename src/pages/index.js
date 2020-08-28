249b6433cdffd243c15ac04327719cb468b23aa0
mathis <mathis.enjolras11@gmail.com>
2020-08-28 23:33:05 +0200

    added crud on step inputs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Layout from '../components/layout/layout';
 import NumberInput from '../components/numberInput/numberInput';
-import { BsChevronDown } from 'react-icons/bs';
+import { BsChevronDown, BsTrashFill } from 'react-icons/bs';
 import SEO from '../components/seo';
 
 const Home = () => {
@@ -9,7 +9,7 @@ const Home = () => {
   const [specialConso, setSpecialConso] = useState(0.60);
   const [special, setSpecial] = useState(0);
   const [showCalcul, setShowCalcul] = useState(false);
-  const [inputList, setInputList] = useState([{ name: "test", value: undefined, isStep: true }]);
+  const [inputList, setInputList] = useState([{ name: "test", value: undefined, isStep: true, isOptional: false }]);
   const [result, setResult] = useState(undefined);
 
   function changeStepConso(value) { setStepConso(Number((stepConso + value).toFixed(2))); }
@@ -17,14 +17,14 @@ const Home = () => {
   function changeSpecial(value) { setSpecial(special + value); }
 
   function generateForm() {
-    let tempInputList = [{ name: "step1", value: undefined, isStep: true }];
+    let tempInputList = [{ name: "step1", value: undefined, isStep: true, isOptional: false }];
     for (let i = 1; i <= special; i++) {
       if (i !== 1) {
-        tempInputList.push({ name: "step" + i, value: undefined, isStep: true })
+        tempInputList.push({ name: "step" + i, value: undefined, isStep: true, isOptional: false })
       }
-      tempInputList.push({ name: "special" + i, value: undefined, isStep: false });
+      tempInputList.push({ name: "special" + i, value: undefined, isStep: false, isOptional: false });
     }
-    tempInputList.push({ name: "step" + (special + 1), value: undefined, isStep: true });
+    tempInputList.push({ name: "step" + (special + 1), value: undefined, isStep: true, isOptional: false });
     setInputList(tempInputList);
     setShowCalcul(true);
   }
@@ -43,6 +43,18 @@ const Home = () => {
       essence += input.isStep ? input.value * stepConso : input.value * specialConso;
     });
     setResult(essence.toFixed(2));
+  }
+
+  function addStep(oldIndex) {
+    let list = [...inputList];
+    list.splice(oldIndex + 1, 0, { name: "step" + list.length, value: undefined, isStep: true, isOptional: true });
+    setInputList(list);
+  }
+
+  function deleteStep(i) {
+    let list = [...inputList];
+    list.splice(i, 1);
+    setInputList(list);
   }
 
   return (
@@ -79,19 +91,31 @@ const Home = () => {
           </div>
           {showCalcul && (
             <div id="calculator">
-              <div className="bg-dark-blue">Début</div>
+              <div className="text-center">
+                <div className="bg-dark-blue">Début</div>
+              </div>
               <form className="custom-form" onSubmit={handleSubmit}>
                 {inputList.map((input, i) => (
                   <div key={i} className="form-group">
                     <label>{input.isStep ? 'Liaison' : 'Spéciale'}</label>
-                    <input type="number" step="0.1" className="form-control" id={i} name={input.name} value={input.value} onChange={e => handleChange(e, i)} required />
+                    <div className="d-flex align-items-center">
+                      <input type="number" step="0.01" className="form-control" id={i} name={input.name} value={input.value} onChange={e => handleChange(e, i)} required />
+                      {input.isStep && !input.isOptional && (<button type="button" className="rounded-crud-btn" onClick={() => addStep(i)}>+</button>)}
+                      {input.isStep && input.isOptional && (
+                        <button type="button" className="rounded-crud-btn" onClick={() => deleteStep(i)}>
+                          <span><BsTrashFill /></span>
+                        </button>
+                      )}
+                    </div>
                   </div>
                 ))}
                 <div className="text-center">
                   <button type="submit" className="rounded-btn">=</button>
                 </div>
               </form>
-              <div className="bg-dark-blue">Fin</div>
+              <div className="text-center">
+                <div className="bg-dark-blue">Fin</div>
+              </div>
             </div>
           )}
           {result && (
